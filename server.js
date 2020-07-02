48338edf5c96809658b1cef455ca1ca32e1475b8
Marxneves <marxkhristie@gmail.com>
2020-07-02 19:59:58 -0300

    Começo da estilizaçao e termino da rota instrutor/index

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const nunjuncks = require('nunjucks');
 const routes = require("./routes")
+
 const server = express();
 
 server.use(express.static('public'))
+server.use(routes);
 
 server.set("view engine","njk");
 
@@ -13,6 +15,6 @@ nunjuncks.configure("views",{
     noCache: true
 })
 
-server.listen(5000, function(){
+server.listen(5001, function(){
     console.log("server is running");
 });
